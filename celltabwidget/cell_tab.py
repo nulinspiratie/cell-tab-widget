3b128c5f7060e48d87716c021facec2aaacdae70
nulinspiratie <serwan.asaad@gmail.com>
2017-09-14 12:41:07 +1000

    added saving/loading

--- celltabwidget/cell_tab.py
+++ celltabwidget/cell_tab.py
@@ -1,7 +1,7 @@
 from time import sleep
 import ipywidgets as widgets
 from IPython.display import display
-from traitlets import Unicode, Int, Bool
+from traitlets import Unicode, Int, Bool, List
 from typing import Union
 from collections import OrderedDict
 
@@ -17,8 +17,14 @@ class CellHiderWidget(widgets.DOMWidget):
     previous_value = Unicode('none').tag(sync=True)
     value = Unicode('default').tag(sync=True)
     min_index = Int(1).tag(sync=True)
-    backup = Bool(False).tag(sync=True)
-    load_backup = Bool(False).tag(sync=True)
+    names = List(Unicode()).tag(sync=True)
+    backup_trigger = Bool(False).tag(sync=True)
+    load_backup_trigger = Bool(False).tag(sync=True)
+
+    base_name = Unicode('').tag(sync=True)
+    show_all_trigger = Bool(False).tag(sync=True)
+    show_ungrouped_trigger = Bool(False).tag(sync=True)
+    show_initialization_trigger = Bool(False).tag(sync=True)
 
 
 class CellTab(widgets.Tab):
@@ -38,23 +44,10 @@ class CellTab(widgets.Tab):
                     self.add_subtab(tab_names)
                 else:
                     self.add_subtab_button()
-                self.set_title(k, name)
+                self.set_title(k, str(name))
 
         # Add plus tab
-        new_tab_text_widget = widgets.Text(placeholder='New tab name')
-        new_tab_text_widget.continuous_update = False
-        new_tab_text_widget.observe(self._handle_tab_button_click, 'value')
-
-        save_backup_button_widget = widgets.Button(description='Backup')
-        save_backup_button_widget.on_click(self.backup)
-
-        load_backup_button_widget = widgets.Button(description='Load backup')
-        load_backup_button_widget.on_click(self.load_backup)
-
-        self.add_child(widgets.VBox([new_tab_text_widget,
-                                     save_backup_button_widget,
-                                     load_backup_button_widget]))
-        self.set_title(len(self.children)-1, '+')
+        self.create_plus_tab()
 
         self.observe(self._handle_tab_change, 'selected_index')
 
@@ -63,7 +56,7 @@ class CellTab(widgets.Tab):
             CellTab.cell_hider_widget = CellHiderWidget()
             self.cell_hider_widget.min_index = min_index
             if names:
-                self.cell_hider_widget.value = list(names)[0]
+                self.cell_hider_widget.value = str(list(names)[0])
 
             display(self.cell_hider_widget)
             display(self)
@@ -86,6 +79,36 @@ class CellTab(widgets.Tab):
         else:
             return self.relative_name
 
+    @property
+    def all_names(self):
+        if self.parent:
+            return self.parent.all_names
+        else:
+            return self.all_relative_names
+
+    @property
+    def all_relative_names(self):
+        names = []
+        for k, child in enumerate(self.children):
+            if isinstance(child, CellTab):
+                names += [f'{self.get_title(k)}.{name}' for name in
+                          child.all_relative_names]
+            else:
+                names.append(self.get_title(k))
+        return names
+
+    @property
+    def base_name(self):
+        if self.parent:
+            my_name = self.parent.get_title(self.parent.children.index(self))
+            if self.parent.base_name:
+                return f'{self.parent.base_name}.{my_name}'
+            else:
+                return my_name
+        else:
+            return ''
+
+
     def _handle_tab_button_click(self, change):
         idx = len(self.children) - 1
         self.add_subtab_button(index=idx)
@@ -100,13 +123,10 @@ class CellTab(widgets.Tab):
             self.cell_hider_widget.previous_value = self.cell_hider_widget.value
             self.cell_hider_widget.value = self.name
 
-
     def _handle_tab_change(self, change):
         tab = change['owner']
         tab_name = tab.get_title(change['new'])
-        if tab_name == '+':
-            return
-        elif self.parent is not None:
+        if self.parent is not None:
             self.parent._handle_tab_change(change)
         else:
             # print('changing value in Python')
@@ -115,6 +135,43 @@ class CellTab(widgets.Tab):
 
     def _handle_subtab_button_click(self, change):
         subtab = self.add_subtab([], index=self.selected_index, replace=True)
+
+    def create_plus_tab(self):
+        new_tab_text_widget = widgets.Text(placeholder='New tab name')
+        new_tab_text_widget.continuous_update = False
+        new_tab_text_widget.observe(self._handle_tab_button_click, 'value')
+
+        save_backup_button_widget = widgets.Button(description='Save Backup')
+        save_backup_button_widget.on_click(self.backup)
+
+        load_backup_button_widget = widgets.Button(description='Load backup')
+        load_backup_button_widget.on_click(self.load_backup)
+
+        backup_widget = widgets.HBox([save_backup_button_widget,
+                                      load_backup_button_widget])
+        tab_VBox_widget = widgets.VBox([new_tab_text_widget,
+                                   backup_widget])
+
+        show_cells_label_widget = widgets.Label(value='Show cells: ')
+
+        all_button_widget = widgets.Button(description='All')
+        all_button_widget.on_click(self.show_all)
+
+        ungrouped_button_widget = widgets.Button(
+            description='Ungrouped')
+        ungrouped_button_widget.on_click(self.show_ungrouped)
+        initialization_button_widget = widgets.Button(
+            description='Initialization')
+        initialization_button_widget.on_click(self.show_initialization)
+
+        cells_VBox_widget = widgets.HBox([show_cells_label_widget,
+                                          all_button_widget,
+                                          ungrouped_button_widget,
+                                          initialization_button_widget])
+
+        self.add_child(widgets.VBox([tab_VBox_widget,
+                                     cells_VBox_widget]))
+        self.set_title(len(self.children)-1, '+')
 
     def get_tab_dict(self):
         if hasattr(self, 'children') and len(self.children) > 1:
@@ -132,10 +189,28 @@ class CellTab(widgets.Tab):
             return []
 
     def backup(self, change):
-        self.cell_hider_widget.backup = not self.cell_hider_widget.backup
+        self.cell_hider_widget.backup_trigger = not \
+            self.cell_hider_widget.backup_trigger
 
     def load_backup(self, change):
-        self.cell_hider_widget.load_backup = not self.cell_hider_widget.load_backup
+        self.cell_hider_widget.load_backup_trigger = not \
+            self.cell_hider_widget.load_backup_trigger
+
+    def show_all(self, change):
+        self.cell_hider_widget.base_name = self.base_name
+        self.cell_hider_widget.show_all_trigger = not \
+            self.cell_hider_widget.show_all_trigger
+
+    def show_ungrouped(self, change):
+        self.cell_hider_widget.names = self.all_names
+        self.cell_hider_widget.show_ungrouped_trigger = not \
+            self.cell_hider_widget.show_ungrouped_trigger
+
+    def show_initialization(self, change):
+        print('showing initialization cells')
+        self.cell_hider_widget.base_name = self.base_name
+        self.cell_hider_widget.show_initialization_trigger = \
+            not self.cell_hider_widget.show_initialization_trigger
 
     def add_subtab_button(self, index=None, replace=False):
         button = widgets.Button(
--- js/lib/main.js
+++ js/lib/main.js
@@ -17,8 +17,12 @@ var CellHiderModel = widgets.DOMWidgetModel.extend({
         previous_value : 'none',
         value : 'default',
         min_index : 1,
-        backup : false,
-        load_backup : false
+        backup_trigger : false,
+        load_backup_trigger : false,
+        base_name : '',
+        show_all_trigger : false,
+        show_ungrouped_trigger : false,
+        show_initialization_trigger : false
     })
 });
 
@@ -29,9 +33,11 @@ var CellHiderView = widgets.DOMWidgetView.extend({
         this.value_changed();
         this.model.on('change:value', this.value_changed, this);
 
-        this.model.on('change:backup', this.backup, this);
-        this.model.on('change:load_backup', this.load_backup, this);
-
+        this.model.on('change:backup_trigger', this.backup, this);
+        this.model.on('change:load_backup_trigger', this.load_backup, this);
+        this.model.on('change:show_all_trigger', this.show_all_base, this);
+        this.model.on('change:show_ungrouped_trigger', this.show_ungrouped, this);
+        this.model.on('change:show_initialization_trigger', this.show_initialization, this);
         this.patch_actions();
     },
 
@@ -40,15 +46,11 @@ var CellHiderView = widgets.DOMWidgetView.extend({
         var value = this.model.get('value');
         console.log(`value changed from ${previous_value} to ${value}`);
 
-        if (previous_value !== 'none' && previous_value !== 'all'){
+        if (previous_value !== 'none' && !previous_value.includes('+')){
             this.tag_cells(previous_value)
         }
 
-        if (value === 'all'){
-            this.show_all_cells()
-        }
-        else{
-            this.hide_all_cells_except(value);
+        if (!value.includes('+')){
             this.show_cells(value)
         }
     },
@@ -132,33 +134,83 @@ var CellHiderView = widgets.DOMWidgetView.extend({
     },
 
     show_cells: function(name) {
+        console.log(`Hiding all cells except beginning with value ${name}`);
+        this.hide_all_cells_except(name);
         console.log(`Showing cells with value ${name}`);
         var cells = Jupyter.notebook.get_cells();
         for (let cell of cells) {
-            if (cell.metadata.cell_tab === name){
+            if (cell.metadata.cell_tab !== undefined && cell.metadata.cell_tab.startsWith(name)){
                 this.show_cell(cell)
             }
         }
     },
 
     show_all_cells: function() {
-        console.log(`Showing all cells`);
-        var cells = Jupyter.notebook.get_cells();
-        for (let cell of cells) {
+        console.log('Showing all cells')
+        for (let cell of Jupyter.notebook.get_cells()) {
             this.show_cell(cell)
         }
     },
 
+    show_initialization: function() {
+        console.log('Showing initialization cells')
+        this.hide_all_cells();
+
+        var base_name = this.model.get('base_name');
+        console.log(`Showing all initialization cells starting with base ${base_name}`)
+        for (let cell of Jupyter.notebook.get_cells()) {
+            if (cell.metadata.init_cell === true && cell.metadata.cell_tab.startsWith(base_name)) {
+                this.show_cell(cell);
+            }
+        }
+    },
+
+    show_all_base: function() {
+        this.hide_all_cells();
+        var base_name = this.model.get('base_name');
+        if (base_name !== '') {
+            console.log(`Showing all cells starting with base ${base_name}`)
+            this.show_cells(base_name)
+        } else {
+            this.show_all_cells()
+        }
+    },
+
+    show_ungrouped: function() {
+        console.log('Showing ungrouped cells');
+        var all_names = this.model.get('names');
+        var min_index = this.model.get('min_index');
+        for (let cell of  Jupyter.notebook.get_cells()) {
+            if (Jupyter.notebook.find_cell_index(cell) <= min_index){
+                // Before the tab cell, ignore
+                continue;
+            }
+
+            if (cell.metadata.cell_tab == 'stop'){
+                // Reached stop cell
+                break;
+            }
+
+            if (all_names.indexOf(cell.metadata.cell_tab) < 0) {
+                console.log(`Displaying ungrouped ${cell.metadata.cell_tab}`)
+                this.show_cell(cell)
+            } else {
+                this.hide_cell(cell)
+            }
+        }
+    },
+
     hide_cell: function(cell) {
-        cell.element.slideUp('fast')
+        console.debug(`Sliding up cell ${cell.get_text()}`)
+        cell.element.slideUp('fast');
         cell.metadata.hidden = true;
     },
 
     show_cell: function(cell) {
-        cell.element.find("div.input").parent("div.cell").css("display", "")
+        console.debug(`Sliding down cell ${cell.get_text()}`)
+        cell.element.slideDown('fast');
         delete cell.metadata.hidden
     },
-
 
     patch_actions: function() {
         console.log(log_prefix, 'patching Jupyter up/down actions');
